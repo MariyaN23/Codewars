d774c8ed7d868f90a886c7aa46f5de745e4e94c7
MariyaN23 <sellaite505@gmail.com>
2023-07-02 22:18:45 +0300

    02.07

---
+++ Pro3.js
@@ -0,0 +1,13 @@
+//Write a function which calculates the average of the numbers in a given list.
+
+function findAverage(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    if (array.length == 0) 
+      {
+        return 0;
+      }
+    else return sum/array.length;
+  }
