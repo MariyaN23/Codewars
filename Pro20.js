f9e1cfaa5304aea108046ef8e3cb241361657e12
MariyaN23 <sellaite505@gmail.com>
2023-07-05 21:33:45 +0300

    20-21

---
+++ Pro20.js
@@ -0,0 +1,13 @@
+/*In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . 
+If limit is a multiple of integer, it should be included as well. 
+There will only ever be positive integers passed into the function, not consisting of 0. 
+The limit will always be higher than the base. */
+
+function findMultiples(integer, limit) {
+    let out = [];
+     for (let i = integer; i <= limit; i = i+integer) {
+       out.push (i);
+       }
+     return out;
+   }
+
