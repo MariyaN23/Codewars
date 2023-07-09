d3d9eb55b1afd22ae3da0f1b03c3b36f91d4109c
MariyaN23 <sellaite505@gmail.com>
2023-07-09 20:14:07 +0300

    dna

---
+++ Pro44.js
@@ -0,0 +1,24 @@
+/* In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". 
+Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. 
+DNA strand is never empty or there is no DNA at all (again, except for Haskell). */
+
+function DNAStrand(dna){
+  let out = "";
+  for (let i=0; i<dna.length; i++) {
+    switch(dna[i]) {
+      case "A":
+      out += "T";
+      break;
+        case "T":
+        out = out + "A";
+        break;
+        case "C":
+        out += "G";
+        break;
+        case "G":
+        out += "C";
+        break;
+        }
+    }
+   return out;
+}
