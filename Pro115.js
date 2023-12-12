c2277fa5d7abf54d96e33c85d6757244628cf579
MariyaN23 <sellaite505@gmail.com>
2023-12-12 20:53:16 +0300

    pro115

---
+++ Pro115.js
@@ -0,0 +1,17 @@
+/* Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+Good Luck!
+
+*/
+
+function doubleChar(str) {
+    let out = ""
+    for (let i=0; i<str.length; i++) {
+      out+=str[i]+str[i]
+    }
+    return out
+  }
